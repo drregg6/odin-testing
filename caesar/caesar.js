f6351ceeb39fbb9a6bc3de3bd170bccfd2ffaee4
Dave Regg <dave.r.regg@gmail.com>
2020-07-24 20:54:27 -0400

    Caesar all tests pass

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,18 +1,34 @@
-module.exports = caesar = (str, k) => {
-  const LETTERS = 'abcdefghijklmnopqrstuvwxyz';
+const caesar = (str, k) => {
   if (str.includes(' ')) {
     let words = str.split(' ');
     for (let i = 0; i < words.length; i++) {
       words[i] = words[i].split('').map(letter => {
-        let temp = (LETTERS.indexOf(letter) + k) % 26;
-        return LETTERS[temp];
+        let charCode;
+        if (letter.toUpperCase() === letter) {
+          charCode = getNewCharCode(letter, k, false);
+        } else {
+          charCode = getNewCharCode(letter, k);
+        }
+        return String.fromCharCode(charCode);
       }).join('');
     }
     return words.join(' ');
   } else {
     return str.split('').map(letter => {
-      let tempIdx = (LETTERS.indexOf(letter) + k) % 26;
-      return LETTERS[tempIdx];
+      let charCode;
+      if (letter.toUpperCase() === letter) {
+        charCode = getNewCharCode(letter, k, false);
+      } else {
+        charCode = getNewCharCode(letter, k);
+      }
+      return String.fromCharCode(charCode);
     }).join('');
   }
 }
+
+const getNewCharCode = (letter, k, lower=true) => {
+  let base = lower ? 97 : 65;
+  return ((letter.charCodeAt() + k - base) % 26) + base
+}
+
+module.exports = caesar;
